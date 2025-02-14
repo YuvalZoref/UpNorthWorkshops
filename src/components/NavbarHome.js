d2c6975d05aba435afa1e866ebe941f6b4bae005
yuvalzoref <yuval_zoref@hotmail.com>
2025-02-14 13:40:28 +0000

    added burger menu to homepage

---
+++ src/components/NavbarHome.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../components/styling/navbarHome.scss';
+import Burger from '../assets/navbarHome/burgerWhite.png';
+
+export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+      <nav id="navbarHome">
+        <div className='navbarDesktopHome'>
+        </div>
+        <img src={Burger} alt='burgerMenu' className='burgerMenuHome' onClick={()=>setShowMenu(!showMenu)} />
+        <div className='navMenuHome' style={{display: showMenu ? 'flex':'none'}}>
+          <div className='navMenuLinksContainerHome'>
+            <Link className='navMenuLinkHome' onClick={()=>setShowMenu(false)} to='../About'>About</Link>
+            <Link className='navMenuLinkHome' onClick={()=>setShowMenu(false)} to='../Team'>Team</Link>
+            <Link className='navMenuLinkHome' onClick={()=>setShowMenu(false)} to='../Contact'>Contact</Link>
+          </div>
+        </div>
+    </nav>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import '../pages/styling/home.scss'
 import logo from '../assets/home/logoUpNorth.png';
 import { Link } from 'react-router-dom';
+import Navbar from '../components/NavbarHome'
 
 export default function Home() {
   return (
     <div id='home'>
-      <Link className='links' to='../About'>
-        <img src={logo} className="home-logo" alt="logo" />
-      </Link>
+    <Navbar />
+      <div className='homeContainer'>
+        <Link className='links' to='../About'>
+          <img src={logo} className="home-logo" alt="logo" />
+        </Link>
+      </div>
     </div>
   )
 }
