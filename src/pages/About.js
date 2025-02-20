cc100068f3721e36e2b0c39b67198aa68271a336
yuvalzoref <yuval_zoref@hotmail.com>
2025-02-20 15:17:31 +0000

    added sections to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,14 +9,54 @@ export default function About() {
       <Navbar />
         <div id='about'>
           <section className='aboutUs'>
-          <h1 className='aboutUsTitle'>About us page title</h1>
-          <p className='aboutUsDescription'>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
-           Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
-           Facere praesentium tenetur harum ipsam ipsum delectus porro optio. Similique.
-           Ad, accusantium cupiditate qui nisi laudantium nostrum fugiat eaque recusandae!
-           Non consequuntur ab voluptate similique eius. Quam blanditiis omnis ipsum.
-          </p>
+            <div className='aboutContainerTop'>
+              <h1 className='aboutUsTitle'>About us</h1>
+              <p className='aboutUsDescription'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+              Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+              Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+              Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+              </p>
+            </div>
+            <div className='aboutContainerMiddle'>
+              <h1 className='workshopsTitle'>Our Workshops</h1>
+              <div className='workshopsSection'>
+                <div className='workshopsSectionSingle'>
+                  <h3 className='workshopsSectionTitle'>Classes</h3>
+                  <p className='workshopsSectionDescr'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                    Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                  </p>
+                </div>
+                <div className='workshopsSectionSingle'>
+                  <h3 className='workshopsSectionTitle'>Teachers</h3>
+                  <p className='workshopsSectionDescr'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                    Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                  </p>
+                </div>
+                <div className='workshopsSectionSingle'>
+                  <h3 className='workshopsSectionTitle'>Vision</h3>
+                  <p className='workshopsSectionDescr'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                    Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='aboutContainerBottom'>
+            <h1 className='whatWeTitle'>What we do</h1>
+              <p className='whatWeDescription'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, nostrum.
+                Totam vitae doloremque, id dolorum beatae ducimus! Esse, quae praesentium!
+                </p>
+            </div>
           </section>
         </div>
       <Footer />
