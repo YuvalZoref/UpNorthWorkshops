d27311a7bd7432dabd0584c8e4e74e5f72b465ec
yuvalzoref <yuval_zoref@hotmail.com>
2025-02-21 11:13:12 +0000

    about page updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import '../components/styling/footer.scss';
+import SpotlightIcon from '../assets/footer/facebookYellow.png'
+import linkedInIcon from '../assets/footer/linkedinYellow.png'
+import instagramIcon from '../assets/footer/instagramYellow.png'
 
 
 
 export default function Footer() {
   return (
     <footer id="footer">
-       Copyright © 2025 | Up North Workshops
+      <div className='row'>
+        <p className='footerCol'>
+            &copy;{new Date().getFullYear()} Up North Workshops | All rights reserved
+        </p>
+        <div className="socialLinksFooter">
+          <a href="/" rel="noreferrer" target="blank"><img src={SpotlightIcon} alt="spotlight" className="socialLinkFooter" /></a>
+          <a href="/" rel="noreferrer" target="blank"><img src={linkedInIcon} alt="spotlight" className="socialLinkFooter" /></a>
+          <a href="/" rel="noreferrer" target="blank"><img src={instagramIcon} alt="instagram" className="socialLinkFooter" /></a>
+        </div>
+      </div>
     </footer>
   )
 }
